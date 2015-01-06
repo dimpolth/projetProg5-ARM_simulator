079276830b5f49f906a63581280cda8c841e191e
Dimpolth <maximax84@live.fr>
2015-01-06 10:12:40 +0100

    Correction de arm_instruction.c. Il compile maintenant !!

--- arm_instruction.c
+++ arm_instruction.c
@@ -71,14 +71,14 @@ static int arm_execute_instruction(arm_core p) {
 		int deroul = 0;
 		switch (type_instr) {
 			case 0 :
-				if(get_bits(instr, 24, 25) == 2 && get_bit(instr, 20) == 0) {
+				if(get_bits(instr, 25, 24) == 2 && get_bit(instr, 20) == 0) {
 					if(get_bit(instr, 4) == 0 || (get_bit(instr, 7) == 0 && get_bit(instr, 4) == 1))
 						deroul = arm_miscellaneous(p, instr);
 				}
 				else {
 					if(get_bit(instr, 4) == 0 || (get_bit(instr, 7) == 0 && get_bit(instr, 4) == 1))
 						deroul = arm_data_processing_shift(p, instr);
-					then
+					else
 						return -1; //Cas normalement impossible à atteindre
 				}
 				break;		
