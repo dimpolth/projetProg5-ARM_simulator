3e8c228fec8e20309a578cd165aad411cff520b5
Dimpolth <maximax84@live.fr>
2015-01-06 10:08:15 +0100

    Fin de la complétion du fichier. Case 0 résolu

--- arm_instruction.c
+++ arm_instruction.c
@@ -30,58 +30,69 @@ Contact: Guillaume.Huard@imag.fr
 
 static int arm_execute_instruction(arm_core p) {
 
-    // Récupération de l'instruction
-    uint32_t PC = arm_read_register(p, 15);
-    uint32_t instr;
-    if (arm_read_word(p, PC, &instr) == -1) return -1;
+	// Récupération de l'instruction
+	uint32_t PC = arm_read_register(p, 15);
+	uint32_t instr;
+	if (arm_read_word(p, PC, &instr) == -1) return -1;
 
-    // Récupération des registre d'état
-    int n, z, c, v;
-    uint32_t cpsr = arm_read_cpsr(p) >> 28;
-    n = (cpsr & 8) >> 3;
-    z = (cpsr & 4) >> 2;
-    c = (cpsr & 2) >> 1;
-    v = (cpsr & 1);
+	// Récupération des registre d'état
+	int n, z, c, v;
+	uint32_t cpsr = arm_read_cpsr(p) >> 28;
+	n = (cpsr & 8) >> 3;
+	z = (cpsr & 4) >> 2;
+	c = (cpsr & 2) >> 1;
+	v = (cpsr & 1);
 
-    // vérification de la condition
-    uint8_t cond = instr >> 28;
-    int res = 1;
-    switch (cond) {
-	case 0 : res = (z == 1); break;
-	case 1 : res = (z == 0); break;
-	case 2 : res = (c == 1); break;
-	case 3 : res = (c == 0); break;
-	case 4 : res = (n == 1); break;
-	case 5 : res = (n == 0); break;
-	case 6 : res = (v == 1); break;
-	case 7 : res = (v == 0); break;
-	case 8 : res = (c == 1 && z == 0); break;
-	case 9 : res = (c == 0 && z == 1); break;
-	case 10 : res = (n == v); break;
-	case 11 : res = (n != v); break;
-	case 12 : res = (z == 0 && n == v); break;
-	case 13 : res = (z == 1 || n != v); break;
-	case 14 : break;
-	default : return -1;
-    }
+	// vérification de la condition
+	uint8_t cond = instr >> 28;
+	int res = 1;
+	switch (cond) {
+		case 0 : res = (z == 1); break;
+		case 1 : res = (z == 0); break;
+		case 2 : res = (c == 1); break;
+		case 3 : res = (c == 0); break;
+		case 4 : res = (n == 1); break;
+		case 5 : res = (n == 0); break;
+		case 6 : res = (v == 1); break;
+		case 7 : res = (v == 0); break;
+		case 8 : res = (c == 1 && z == 0); break;
+		case 9 : res = (c == 0 && z == 1); break;
+		case 10 : res = (n == v); break;
+		case 11 : res = (n != v); break;
+		case 12 : res = (z == 0 && n == v); break;
+		case 13 : res = (z == 1 || n != v); break;
+		case 14 : break;
+		default : return -1;
+	}
     
-    // Execution (ou pas) de l'instruction
-    if (res) {
-	int type_instr = (instr && (7 << 25)) >> 25;
-	int deroul = 0;
-        switch (type_instr) {
-   	   case 0 : deroul = arm_data_processing_shift(p, instr); break;
-	   case 1 : deroul = arm_data_processing_immediate_msr(p, instr); break;
-           case 2 : deroul = arm_load_store(p, instr); break;
-           case 3 : break;
-           case 4 : deroul = arm_load_store_multiple(p, instr); break;
-	   case 5 : deroul = arm_branch(p, instr); break;
-           case 6 : deroul = arm_coprocessor_load_store(p, instr); break;
-           case 7 : deroul = arm_coprocessor_others_swi(p, instr); break;
-	   default : return -1;
-	} 
-	if (deroul == -1) return -1;
-    }
+  // Execution (ou pas) de l'instruction
+	if (res) {
+		int type_instr = (instr && (7 << 25)) >> 25;
+		int deroul = 0;
+		switch (type_instr) {
+			case 0 :
+				if(get_bits(instr, 24, 25) == 2 && get_bit(instr, 20) == 0) {
+					if(get_bit(instr, 4) == 0 || (get_bit(instr, 7) == 0 && get_bit(instr, 4) == 1))
+						deroul = arm_miscellaneous(p, instr);
+				}
+				else {
+					if(get_bit(instr, 4) == 0 || (get_bit(instr, 7) == 0 && get_bit(instr, 4) == 1))
+						deroul = arm_data_processing_shift(p, instr);
+					then
+						return -1; //Cas normalement impossible à atteindre
+				}
+				break;		
+			case 1 : deroul = arm_data_processing_immediate_msr(p, instr); break;
+			case 2 : deroul = arm_load_store(p, instr); break;
+			case 3 : deroul = arm_load_store(p, instr); break;
+			case 4 : deroul = arm_load_store_multiple(p, instr); break;
+			case 5 : deroul = arm_branch(p, instr); break;
+			case 6 : deroul = arm_coprocessor_load_store(p, instr); break;
+			case 7 : deroul = arm_coprocessor_others_swi(p, instr); break;
+			default : return -1; //Cas normalement impossible à atteindre
+		} 
+		if (deroul == -1) return -1;
+	}
 
     // Incrémentation PC
     arm_write_register(p, 15, PC+4);
