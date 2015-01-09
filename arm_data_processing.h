00781be8b06993102c49a814c4fbd9059945d612
dimpolth <maximax84@live.fr>
2015-01-09 14:17:07 +0100

    Oubli du .h. Je suis un boulet.

--- arm_data_processing.h
+++ arm_data_processing.h
@@ -26,8 +26,8 @@ Contact: Guillaume.Huard@imag.fr
 #include "arm_core.h"
 
 int arm_data_processing_shift(arm_core p, uint32_t ins);
-void opChoice(arm_core p, uint32_t ins, uint32_t cpsr, int32_t shifter_operand, int shifter_carry_out);
-int32_t rightRotate(int32_t shift_imm, int32_t rm);
+void opChoice(arm_core p, uint32_t ins, uint32_t cpsr, uint32_t shifter_operand, int shifter_carry_out);
+uint32_t rightRotate(uint32_t shift_imm, uint32_t rm);
 int arm_data_processing_immediate(arm_core p, uint32_t ins);
 
 #endif
